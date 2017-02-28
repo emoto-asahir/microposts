8e160b98a99dae09b489206e7f12d9deaaf21d04
emoto a <s1620606@u.tsukuba.ac.jp>
2017-02-28 13:41:41 +0000

    change home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,9 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(3)
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(5)
+    else
+      @feed_items = Micropost.all.order(created_at: :desc).page(params[:page]).per(5)
     end
   end
 end
